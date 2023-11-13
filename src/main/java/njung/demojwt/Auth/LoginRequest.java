441a5a4d6c17e7f59d17c2d0ea0df590a1b8af38
Nico <nspinettojung@gmail.com>
2023-11-13 14:08:54 -0300

    Initial commit

---
+++ src/main/java/njung/demojwt/Auth/LoginRequest.java
@@ -0,0 +1,18 @@
+package njung.demojwt.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    
+    String username;
+    String password;
+
+    
+}
---
+++ src/main/java/njung/demojwt/User/Role.java
@@ -0,0 +1,6 @@
+package njung.demojwt.User;
+
+public enum Role {
+    ADMIN,
+    USER
+}
